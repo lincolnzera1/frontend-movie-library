5a23770c838442832947124b1d1b2d850eefaab8
lincolnzera1 <guilherme.lincoln.lima.11@gmail.com>
2023-12-12 03:41:06 -0300

    Adjustments to the menu

--- src/components/HeaderBar/MenuItems.tsx
+++ src/components/HeaderBar/MenuItems.tsx
@@ -12,22 +12,22 @@ const MenuItems = () => {
     },
     {
       label: "Catálogo de filmes",
-      icon: "pi pi-fw pi-user",
+      icon: "pi pi-fw pi-align-center",
       command: () => navigate("/filmes"),
     },
     {
       label: "Adicionar Filmes",
-      icon: "pi pi-fw pi-briefcase",
+      icon: "pi pi-fw pi-plus",
       command: () => navigate("/filmes/adicionar"),
     },
     {
       label: "Lista de usuários",
-      icon: "pi pi-fw pi-pencil",
+      icon: "pi pi-fw pi-align-center",
       command: () => navigate("/usuarios"),
     },
     {
       label: "Cadastrar usuário",
-      icon: "pi pi-fw pi-user",
+      icon: "pi pi-fw pi-plus",
       command: () => navigate("/usuarios/adicionar"),
     },
   ];
